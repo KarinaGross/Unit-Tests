590a72852f03fbb44f395fb8757c6eada5047ecc
Karina <karina.dragan4uk@yandex.ru>
2023-10-08 19:41:01 +0300

    homework6

--- Test/hw6/ListComparatorTest.java
+++ Test/hw6/ListComparatorTest.java
@@ -1,4 +1,39 @@
+package hw6;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ListComparatorTest {
-  
+    @Test
+    public void testFirstListHasGreaterAverage() {
+        assertEquals("Первый список имеет большее среднее значение",
+                ListComparator.compare(Arrays.asList(1, 2, 3, 4, 5),
+                        Arrays.asList(1, 2, 3)));
+    }
+
+    @Test
+    public void testSecondListHasGreaterAverage() {
+        assertEquals("Второй список имеет большее среднее значение",
+                ListComparator.compare(Arrays.asList(1, 2, 3),
+                        Arrays.asList(1, 2, 3, 4, 5, 6)));
+    }
+
+    @Test
+    public void testAveragesAreEqual() {
+        assertEquals("Средние значения равны",
+                ListComparator.compare(Arrays.asList(1, 2, 3, 4, 5),
+                        Arrays.asList(1, 2, 3, 4, 5)));
+    }
+
+    @Test
+    public void testEmptyLists() {
+        assertEquals("Средние значения равны",
+                ListComparator.compare(Collections.emptyList(),
+                        Collections.emptyList()));
+    }
 }
